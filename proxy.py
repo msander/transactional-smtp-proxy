7cfe741abf88a17b70f8394734496c6da1ef1755
Marcel Sander <marcel.sander@actidoo.com>
2020-06-11 21:06:56 +0200

    add logging + remove header

--- proxy.py
+++ proxy.py
@@ -10,12 +10,14 @@ log.setLevel(logging.INFO)
 
 SMTP_HOST = os.environ["SMTP_HOST"]
 SMTP_PORT = int(os.environ["SMTP_PORT"])
+LOG_CONTENT = bool(os.environ.get("LOG_CONTENT", False))
 
 class TransactionalHandler(aiosmtpd.handlers.AsyncMessage):
     async def handle_message(self, message: email.message.EmailMessage):
         transactional_mail = False
         if message.get("Remove-List-Unsubscribe", None):
             del message["List-Unsubscribe"]
+            del message["Remove-List-Unsubscribe"]
             transactional_mail = True
 
         del message['X-Peer']
@@ -28,3 +30,12 @@ class TransactionalHandler(aiosmtpd.handlers.AsyncMessage):
             log.info(f'Transactional Mail has been sent to {",".join(message.get_all("to", []))}')
         else:
             log.info(f'Non-Transactional Mail has been sent to {",".join(message.get_all("to", []))}')
+
+        if LOG_CONTENT:
+            for part in message.walk():
+                # each part is a either non-multipart, or another multipart message
+                # that contains further parts... Message is organized like a tree
+                if part.get_content_type() == 'text/plain':
+                    log.info(part.get_payload())
+                if part.get_content_type() == 'text/html':
+                    log.info(part.get_payload())
